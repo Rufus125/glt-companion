a28432ce37bff3d8715c4a08d838c6a32bf92713
Rufus <Rufus125@users.noreply.github.com>
2019-03-29 14:36:01 +0100

    Adds mapping table Room - Building

--- app/src/main/java/at/linuxtage/companion/model/Building.java
+++ app/src/main/java/at/linuxtage/companion/model/Building.java
@@ -2,33 +2,84 @@ package at.linuxtage.companion.model;
 
 import android.text.TextUtils;
 
-public enum Building {
-	J, K, H, U, AW, Unknown;
-//TODO mapping of Building to room for GLT
-	public static Building fromRoomName(String roomName) {
-		if (!TextUtils.isEmpty(roomName)) {
-			switch (Character.toUpperCase(roomName.charAt(0))) {
-			case 'K':
-				return K;
-			case 'H':
-				return H;
-			case 'U':
-				return U;
-			}
-			if (roomName.regionMatches(true, 0, "AW", 0, 1)) {
-				return AW;
-			}
-			if ("Janson".equalsIgnoreCase(roomName)) {
-				return J;
-			}
-			if ("Ferrer".equalsIgnoreCase(roomName)) {
-				return H;
-			}
-			if ("Chavanne".equalsIgnoreCase(roomName) || "Lameere".equalsIgnoreCase(roomName) || "Guillissen".equalsIgnoreCase(roomName)) {
-				return U;
-			}
-		}
+import at.linuxtage.companion.R;
 
-		return Unknown;
-	}
+public enum Building {
+    I12("Inffeldgasse 12"), Unknown("Unknown"), I18EG("Inffeldgasse 16a EG"), I25fEG("Inffeldgasse 25f EG"), I10("Inffeldgasse 10"), I25d("Inffeldgasse 25d"), I251OG("Inffeldgasse 25 1. OG"), I12EG("Inffeldgasse 12"), I16bK("Inffeldgasse 16b Keller"), I18("Inffeldgasse 18");
+    private final String name;
+
+    Building(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        if (this.equals(Unknown)) {
+            //return getString(R.string.unknown);
+        }
+        return this.name;
+    }
+
+    //TODO mapping of Building to room for GLT
+    public static Building fromRoomName(String roomName) {
+        Building building = Unknown;
+        if (!TextUtils.isEmpty(roomName)) {
+            switch (roomName) {
+                case "HF01092":
+                    building = I12;
+                    break;
+
+                case "HSTP056J":
+                case "i1":
+                case "HSEG058J":
+                case "IFEG042":
+                    building = I18EG;
+                    break;
+
+                case "MFEG210":
+                    building = I25fEG;
+                    break;
+
+                case "SZ01070":
+                case "SZ02053":
+                    building = I10;
+                    break;
+
+                case "i3":
+                case "i4":
+                case "i5":
+                case "i6":
+                    building = I25d;
+                    break;
+
+                case "i11":
+                case "ICK1002H":
+                case "i12":
+                case "ICK1130H":
+                case "i13":
+                case "ICK1120H":
+                    building = I16bK;
+                    break;
+
+                case "i14":
+                case "i15":
+                    building = I18;
+                    break;
+
+
+                case "i2":
+                case "HFEG038J":
+                    building = I12EG;
+                    break;
+
+                case "i7":
+                case "MD01168F":
+                    building = I251OG;
+                    break;
+            }
+        }
+        return building;
+    }
 }
+
